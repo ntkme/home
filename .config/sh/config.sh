c81879e1050ef7302e9c7829600fd4d494e22c70
なつき <i@ntk.me>
2016-03-04 11:39:49 -0800

    Update linuxbrew prefix

--- .config/sh/config.sh
+++ .config/sh/config.sh
@@ -8,8 +8,8 @@
 # Prepend /usr/local/bin for Homebrew
 test -x /usr/local/bin/brew && export PATH=/usr/local/bin:`echo ":$PATH:" | sed -e "s:\:/usr/local/bin\::\::g" -e "s/^://" -e "s/:$//"`
 
-# Prepend $HOME/.linuxbrew/bin for Linuxbrew
-test -x "$HOME/.linuxbrew/bin/brew" && export PATH="$HOME/.linuxbrew/bin:$HOME/.linuxbrew/sbin:$PATH"
+# Prepend /opt/local/bin for Linuxbrew
+test -x /opt/local/bin/brew && export PATH=/opt/local/bin:/opt/local/sbin:$PATH
 
 # Alias
 alias ll='ls -lh'
@@ -20,6 +20,6 @@ case $(uname) in
   Darwin)
     ;;
   Linux)
-    alias ls="command ls --color=auto"
+    alias ls='command ls --color=auto'
     ;;
 esac
