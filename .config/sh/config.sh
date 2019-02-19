0c4b7f85c49315fc26ec5a74a2246f81b7936aa2
なつき <i@ntk.me>
2019-02-19 11:46:34 +0000

    Setup PATH with `brew shellenv`

--- .config/sh/config.sh
+++ .config/sh/config.sh
@@ -5,25 +5,18 @@
 # `bash`
 # `zsh`
 
-# Prepend /usr/local/bin for Homebrew
-test -x /usr/local/bin/brew && export PATH=/usr/local/bin:`echo ":$PATH:" | sed -e "s:\:/usr/local/bin\::\::g" -e "s/^://" -e "s/:$//"`
-
-# Prepend /home/linuxbrew/.linuxbrew/bin for Linuxbrew
-if test -x /home/linuxbrew/.linuxbrew/bin/brew; then
-  export PATH=/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:`echo ":$PATH:" | sed -e "s:\:/home/linuxbrew/.linuxbrew/bin\::\::g" -e "s:\:/home/linuxbrew/.linuxbrew/sbin\::\::g" -e "s/^://" -e "s/:$//"`
-  export MANPATH=/home/linuxbrew/.linuxbrew/share/man:$MANPATH
-  export INFOPATH=/home/linuxbrew/.linuxbrew/share/info:$INFOPATH
-fi
-
-# Alias
-alias ll='ls -lh'
-alias la='ls -lAh'
-alias grep='grep --color=auto'
-
 case $(uname) in
   Darwin)
+    test -x /usr/local/bin/brew && eval "$(/usr/local/bin/brew shellenv)"
     ;;
   Linux)
+    test -x /home/linuxbrew/.linuxbrew/bin/brew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+    test -x "$HOME/.linuxbrew/bin/brew" && eval "$("$HOME/.linuxbrew/bin/brew" shellenv)"
+
     alias ls='ls --color=auto'
     ;;
 esac
+
+alias ll='ls -lh'
+alias la='ls -lAh'
+alias grep='grep --color=auto'
