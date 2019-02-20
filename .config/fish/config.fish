560cf151d9ef0aa03dd9f7a6ebc065c8b3e995e6
なつき <i@ntk.me>
2019-02-20 10:25:14 -0800

    Load `brew shellenv` once for fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,10 @@
 fish_env ~/.env
 set -gx fish_greeting
-test -x /usr/local/bin/brew; and set -gx fish_user_paths /usr/local/bin
+
+switch (uname)
+  case 'Darwin'
+    set -q $HOMEBREW_PREFIX; and test -x /usr/local/bin/brew; and env SHELL=/bin/fish /usr/local/bin/brew shellenv | source
+  case 'Linux'
+    set -q $HOMEBREW_PREFIX; and test -x /home/linuxbrew/.linuxbrew/bin/brew; and env SHELL=/bin/fish /home/linuxbrew/.linuxbrew/bin/brew shellenv | source
+    set -q $HOMEBREW_PREFIX; and test -x ~/.linuxbrew/bin/brew; and env SHELL=/bin/fish ~/.linuxbrew/bin/brew shellenv | source
+end
