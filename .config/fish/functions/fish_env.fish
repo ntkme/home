7eefcebe085902a71da3e1b2ce0af52093b9006e
なつき <i@ntk.me>
2019-02-20 07:30:46 +0000

    Fix fish_env

--- .config/fish/functions/fish_env.fish
+++ .config/fish/functions/fish_env.fish
@@ -1,5 +1,5 @@
 function fish_env -d "Load environment variables from POSIX .env"
-  set -l pattern '^[:space:]*\(export[:space:][:space:]*\)\{0,1\}\([a-zA-Z_][a-zA-Z0-9_]*\)=\(.*\)[:space:]*$'
+  set -l pattern '^[[:space:]]*\(export[[:space:]][[:space:]]*\)\{0,1\}\([a-zA-Z_][a-zA-Z0-9_]*\)=\(.*\)[[:space:]]*$'
   set -l file
   for file in $argv
     set -l line
