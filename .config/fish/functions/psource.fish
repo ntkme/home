95d2a0e814192b01d3951f33204873bb3f74d927
なつき <i@ntk.me>
2019-07-24 16:11:45 -0700

    Update psource.fish

--- .config/fish/functions/psource.fish
+++ .config/fish/functions/psource.fish
@@ -1,5 +1,5 @@
 function psource -d 'Execute POSIX shell script in the current shell context'
   begin
-    /usr/bin/env -u HOME -- /bin/sh -c '. "$@"; { XDG_CONFIG_HOME="${0%/*/*}/etc" "$0" -c "set -gxL" | sed -e "/^\(HOME\|LC_CTYPE\|PATH\|PWD\|SHLVL\|USER\|XDG_CONFIG_HOME\) /d" -e "s/^/set -gx /"; } >&3' (status fish-path) $argv 3>| source
+    /usr/bin/env -u HOME -- /bin/sh -c '. "$@"; XDG_CONFIG_HOME="${0%/*/*}/etc" "$0" -c "set -gxL" | sed -e "/^\(HOME\|LC_CTYPE\|PATH\|PWD\|SHLVL\|USER\|XDG_CONFIG_HOME\) /d" -e "s/^/set -gx /" >&3' (status fish-path) $argv 3>| source
   end | cat
 end
