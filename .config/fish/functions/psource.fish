74a17bac5d371bd68d56fbb9b39118371c4aa8ab
なつき <i@ntk.me>
2019-08-01 12:05:48 -0700

    Make sed syntax bsd compatible

--- .config/fish/functions/psource.fish
+++ .config/fish/functions/psource.fish
@@ -1,3 +1,3 @@
 function psource -d 'Execute POSIX shell script in the current shell context'
-  /bin/sh -c '. "$@"; XDG_CONFIG_HOME="${0%/*/*}/etc" "$0" -c "set -gxL" | sed -e "/^\(HOME\|LC_CTYPE\|PATH\|PWD\|SHLVL\|USER\|XDG_CONFIG_HOME\) /d" -e "s/^/set -gx /" >&3' (status fish-path) $argv 3>| source
+  /bin/sh -c '. "$@"; XDG_CONFIG_HOME="${0%/*/*}/etc" "$0" -c "set -gxL" | sed -e "/^HOME /d" -e "/^LC_CTYPE /d" -e "/^PATH /d" -e "/^PWD /d" -e "/^SHLVL /d" -e "/^USER /d" -e "/^XDG_CONFIG_HOME /d" -e "s/^/set -gx /" >&3' (status fish-path) $argv 3>| source
 end
