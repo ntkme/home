2b4983a9844fc695dfe1d4fe1808da5b6ab71d28
なつき <i@ntk.me>
2020-07-07 01:38:44 -0700

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,17 @@
-psource ~/.env
-
 set -gx fish_greeting
+
+psource ~/.env
 
 switch (uname)
   case 'Darwin'
-    set -q $HOMEBREW_PREFIX; and test -x /usr/local/bin/brew; and env SHELL=/bin/fish /usr/local/bin/brew shellenv | source
+    not set -q HOMEBREW_PREFIX; and test -x /usr/local/bin/brew; and SHELL=/bin/fish /usr/local/bin/brew shellenv | source
+
+    not set -q DOCKER_HOST; and type -q docker-machine; and docker-machine env 2>/dev/null | source
+
+    not set -q JAVA_HOME; and set -gx JAVA_HOME (/usr/libexec/java_home 2>/dev/null); or set -e JAVA_HOME
+
   case 'Linux'
-    set -q $HOMEBREW_PREFIX; and test -x /home/linuxbrew/.linuxbrew/bin/brew; and env SHELL=/bin/fish /home/linuxbrew/.linuxbrew/bin/brew shellenv | source
-    set -q $HOMEBREW_PREFIX; and test -x ~/.linuxbrew/bin/brew; and env SHELL=/bin/fish ~/.linuxbrew/bin/brew shellenv | source
+    not set -q HOMEBREW_PREFIX; and test -x ~/.linuxbrew/bin/brew; and SHELL=/bin/fish ~/.linuxbrew/bin/brew shellenv | source
+    not set -q HOMEBREW_PREFIX; and test -x /home/linuxbrew/.linuxbrew/bin/brew; and SHELL=/bin/fish /home/linuxbrew/.linuxbrew/bin/brew shellenv | source
+
 end
