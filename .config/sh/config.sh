1bce807776c537e19166017e8765ffab64a31985
なつき <i@ntk.me>
2020-07-07 00:06:00 -0700

    Update config.sh

--- .config/sh/config.sh
+++ .config/sh/config.sh
@@ -12,16 +12,15 @@ set -o vi
 
 case "$(uname)" in
   Darwin)
-    test -z ${HOMEBREW_PREFIX:+null} && test -x /usr/local/bin/brew && eval "$(SHELL=/bin/sh /usr/local/bin/brew shellenv)"
+    test -z "$HOMEBREW_PREFIX" && test -x /usr/local/bin/brew && eval "$(SHELL=/bin/sh /usr/local/bin/brew shellenv)"
 
-    JAVA_HOME="$(/usr/libexec/java_home 2>/dev/null)"
-    test -n "$JAVA_HOME" && export JAVA_HOME || unset JAVA_HOME
+    test -z "$DOCKER_HOST" && command -v docker-machine >/dev/null && eval "$(docker-machine env 2>/dev/null)"
 
-    command -v docker-machine >/dev/null && eval $(docker-machine env 2>/dev/null)
+    test -z "$JAVA_HOME" && JAVA_HOME=$(/usr/libexec/java_home 2>/dev/null) && export JAVA_HOME
     ;;
   Linux)
-    test -z ${HOMEBREW_PREFIX:+null} && test -x /home/linuxbrew/.linuxbrew/bin/brew && eval "$(SHELL=/bin/sh /home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-    test -z ${HOMEBREW_PREFIX:+null} && test -x ~/.linuxbrew/bin/brew && eval "$(SHELL=/bin/sh ~/.linuxbrew/bin/brew shellenv)"
+    test -z "$HOMEBREW_PREFIX" && test -x ~/.linuxbrew/bin/brew && eval "$(SHELL=/bin/sh ~/.linuxbrew/bin/brew shellenv)"
+    test -z "$HOMEBREW_PREFIX" && test -x /home/linuxbrew/.linuxbrew/bin/brew && eval "$(SHELL=/bin/sh /home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
     alias ls='ls --color=auto'
     ;;
