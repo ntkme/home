db8439fbebf67c1d1abd654a2aa3f836edc483f8
なつき <i@ntk.me>
2023-03-24 11:52:58 -0700

    Update brew config files

---
+++ .Brewfile
@@ -0,0 +1,8 @@
+brew "ag"
+brew "fd"
+brew "fzf"
+brew "gcc" if OS.linux?
+brew "node"
+brew "sqlite"
+brew "tmux"
+brew "vim"
--- .config/sh/config.sh
+++ .config/sh/config.sh
@@ -9,20 +9,15 @@
 set -o vi
 
 . ~/.env
+. ~/.config/sh/functions/brew-shellenv
 
 case "$(uname)" in
   Darwin)
-    test -z "${HOMEBREW_PREFIX+_}" && test -x /opt/homebrew/bin/brew && eval "$(SHELL=/bin/sh /opt/homebrew/bin/brew shellenv)"
-    test -z "${HOMEBREW_PREFIX+_}" && test -x /usr/local/bin/brew && eval "$(SHELL=/bin/sh /usr/local/bin/brew shellenv)"
-
     test -z "${DOCKER_HOST+_}" && command -v docker-machine >/dev/null && eval "$(docker-machine env 2>/dev/null)"
 
     test -z "${JAVA_HOME+_}" && JAVA_HOME=$(/usr/libexec/java_home 2>/dev/null) && export JAVA_HOME || unset JAVA_HOME
     ;;
   Linux)
-    test -z "${HOMEBREW_PREFIX+_}" && test -x ~/.linuxbrew/bin/brew && eval "$(SHELL=/bin/sh ~/.linuxbrew/bin/brew shellenv)"
-    test -z "${HOMEBREW_PREFIX+_}" && test -x /home/linuxbrew/.linuxbrew/bin/brew && eval "$(SHELL=/bin/sh /home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-
     alias ls='ls --color=auto'
     ;;
 esac
